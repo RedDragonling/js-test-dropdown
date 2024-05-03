dcd5298ee5998a07b9db56dc171d016b9836872f
Eddie Nilsen <eddienilsen0506@gmail.com>
2024-05-03 10:10:39 +0200

    comments and commented out redundant code

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const redImg = 'imgRed.png';
 const blueImg = 'imgBlue.png';
 const yellowImg = 'imgYellow.png';
 
-const selectElement = document.querySelector('#select-color');
-const selectedValue = selectElement.value;
-console.log(selectedValue);
+// const selectElement = document.querySelector('#select-color');
+// const selectedValue = selectElement.value;
+// console.log(selectedValue);
 
+// this is how to change img src from select+value update
 addEventListener("change", (event) =>{
   console.log(event.target.value);
   if(event.target.value == 'red'){
