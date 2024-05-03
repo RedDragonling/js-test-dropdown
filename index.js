7815c1d4120d5e9c05fca07751070509a766d44c
Eddie Nilsen <eddienilsen0506@gmail.com>
2024-05-03 10:05:33 +0200

    Test to make JS change img src on change

---
+++ index.js
@@ -0,0 +1,24 @@
+const imageElement = document.getElementById('image-js-test');
+const redImg = 'imgRed.png';
+const blueImg = 'imgBlue.png';
+const yellowImg = 'imgYellow.png';
+
+const selectElement = document.querySelector('#select-color');
+const selectedValue = selectElement.value;
+console.log(selectedValue);
+
+addEventListener("change", (event) =>{
+  console.log(event.target.value);
+  if(event.target.value == 'red'){
+    console.log('I am red')
+    imageElement.src = redImg;
+  } else if(event.target.value == 'blue'){
+    console.log('I am blue')
+    imageElement.src = blueImg;
+  } else if(event.target.value == 'yellow'){
+    console.log('I am yellow')
+    imageElement.src = yellowImg;
+  } else {
+    console.log('Error')
+  };
+});
